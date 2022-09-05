59d62b2da092b8b62e737f3bff703a61423b76af
Anshuman <anshuman8swain@gmail.com>
2022-09-05 08:33:44 +0530

    done

---
+++ README.md
@@ -0,0 +1,18 @@
+## CRUD using Apollo-server-express and GraphQL
+
+
+### Install all dependency
+```bash
+npm i
+```
+### Run the application
+```bash
+npm run dev
+```
+#### .env sample
+```.env
+PORT = YOUR_PORT
+MODE = APPLICATION_MODE-DEV/PROD
+BASE_URL = YOUR_BASE_URL
+SECRET = ANY_SECRET
+```
--- src/index.js
+++ src/index.js
@@ -1,79 +1,81 @@
-import {join} from 'path';
+import { join } from "path";
 import express from "express";
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import * as AppModels from './models';
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import * as AppModels from "./models";
 import { success, error } from "consola";
-import { PORT, IN_PROD,DB } from "./config";
-import {resolvers,typeDefs}   from './graphql';
-import AuthMiddleware from './middlewares/auth';
+import { PORT, IN_PROD, DB } from "./config";
+import { resolvers, typeDefs } from "./graphql";
+import AuthMiddleware from "./middlewares/auth";
 import { ApolloServer } from "apollo-server-express";
 // import { schemaDirectives } from './graphql/directives';
 
 
 const startApp = async () => {
-  try{
-
+  try {
     // initialize express application
-  const app = express();
-  app.use(AuthMiddleware);
-  app.use(bodyParser.json());
-  app.use(express.static(join(__dirname,'./Uploads')))
+    const app = express();
+    app.use(AuthMiddleware);
+    app.use(bodyParser.json());
+    app.use(express.static(join(__dirname, "./Uploads")));
 
-  app.get('/post',async(req,res)=>{
-    let {Post}=AppModels;
-    let {page,limit} = req.query;
+    // app.get('/post',async(req,res)=>{
+    //   let {Post}=AppModels;
+    //   let {page,limit} = req.query;
 
-    const myCustomLabels = {
-      totalDocs: 'postCount',
-      docs: 'posts',
-      limit: 'perPage',
-      page: 'currentPage',
-      nextPage: 'next',
-      prevPage: 'prev',
-      totalPages: 'pageCount',
-      pagingCounter: 'slNo',
-      meta: 'paginator',
-    };
+    //   const myCustomLabels = {
+    //     totalDocs: 'postCount',
+    //     docs: 'posts',
+    //     limit: 'perPage',
+    //     page: 'currentPage',
+    //     nextPage: 'next',
+    //     prevPage: 'prev',
+    //     totalPages: 'pageCount',
+    //     pagingCounter: 'slNo',
+    //     meta: 'paginator',
+    //   };
 
-    const options = {
-      page: page || 1,
-      limit: limit || 10,
-      customLabels: myCustomLabels,
-    };
+    //   const options = {
+    //     page: page || 1,
+    //     limit: limit || 10,
+    //     customLabels: myCustomLabels,
+    //   };
 
-    let posts=await Post.paginate({}, options);
+    //   let posts=await Post.paginate({}, options);
 
-    return res.json(posts);
+    //   return res.json(posts);
 
-  });
+    // });
 
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    // schemaDirectives,
-    playground: !IN_PROD,
-    context: ({req})=>{
-      let {isAuth, user}=req;
-      return{req,isAuth, user,...AppModels};
-    },
-  });
+    const apolloServer = new ApolloServer({
+      typeDefs,
+      resolvers,
+      // schemaDirectives,
+      playground: !IN_PROD,
+      context: ({ req }) => {
+        let { isAuth, user } = req;
+        return { req, isAuth, user, ...AppModels };
+      },
+    });
 
-  await mongoose.connect(DB,{useNewUrlParser:true,useUnifiedTopology:true});
-  success(`Connected to MongoDB at: ${DB}`);
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    success(`Connected to MongoDB at: ${DB}`);
 
-  await apolloServer.start();
+    await apolloServer.start();
 
-  // inject appolo server middleware to express app
-  await apolloServer.applyMiddleware({ app }); // apply apollo server to express app, u can change the graphql path as {app,path: '/graphql'}
+    // inject appolo server middleware to express app
+    await apolloServer.applyMiddleware({ app }); // apply apollo server to express app, u can change the graphql path as {app,path: '/graphql'}
 
-  app.listen({ port: PORT }, () =>
-    success({
-      message: `Server started at http://localhost:${PORT}`,
-      badge: true,
-    })
-  );
-  }catch(err){
+    app.listen({ port: PORT }, () =>
+      success({
+        message: `Server started at http://localhost:${PORT}`,
+        badge: true,
+      })
+    );
+  } catch (err) {
     error(err);
   }
 };
